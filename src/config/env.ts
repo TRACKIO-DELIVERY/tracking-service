da3e75d554a08f31720f3080775b5e5eb1a15e3a
Karen Dantas <karennobre74@gmail.com>
2025-05-22 13:41:36 -0300

    feat: use geocode api service

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,4 +4,5 @@ export const env = {
   PORT: process.env.PORT || 3000,
   DATABASE_URL: process.env.DATABASE_URL!,
   RABBITMQ_URL: process.env.RABBITMQ_ULR!,
+  GEOCODING_API_KEY: process.env.GEOCODING_API_KEY!,
 };
--- src/services/GeocodeService.ts
+++ src/services/GeocodeService.ts
@@ -1,1 +1,22 @@
-//usar api geocoding para traduzir endereços para coords
+import axios from 'axios';
+import { env } from '../config/env.js';
+
+export class GeocodeService {
+  static async addressToCoords(
+    address: string,
+  ): Promise<{ lat: number; lng: number }> {
+    const apiKey = env.GEOCODING_API_KEY;
+    const response = await axios.get(
+      'https://api.opencagedata.com/geocode/v1/json',
+      {
+        params: {
+          q: address,
+          key: apiKey,
+        },
+      },
+    );
+
+    const { lat, lng } = response.data.results[0].geometry;
+    return { lat, lng };
+  }
+}
